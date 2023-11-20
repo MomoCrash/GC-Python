aecdc11b265698df38f56b6cb3f531662f495bc6
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2023-11-20 15:35:34 +0100

    PFC Textes guez

--- PFS.py
+++ PFS.py
@@ -6,13 +6,18 @@ rejouer: bool = True
 while rejouer : 
     pcChoice = random.choice(pc)
     player: str = input("pierre, feuille ou ciseaux ?")
+    
+    while player != "pierre" and player !="feuille" and player!="ciseaux":
+        player: str = input("pierre, feuille ou ciseaux ?")  
         
     for i,j in valeurs.items():
         if player==i and pcChoice==j:
             print("pc chose",pcChoice,"you won")
         elif player==j and pcChoice==i:
-            print("pc chose",pcChoice,"you lose")    
+            print("pc chose",pcChoice,"you lose")   
+             
     if player == pcChoice :
          print("pc chose",pcChoice,"draw")
+         
     rejouer = input("veux-tu rejouer ? y/n") == "y"
     
