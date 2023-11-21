1c8d608bb408500be09a71379c982cf21b1cb56f
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2023-11-21 10:08:47 +0100

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,4 +1,10 @@
+def verif_taille(demande):
+    while len(var) !=5:
+        var= input(demande)
+
+
 mot = "pates"
-motJoueur: str= input("Écrivez un mot de 5 lettres : ")
+motJoueur = verif_taille("Écrivez un mot de 5 lettres")
+
 
 for i in mot 
