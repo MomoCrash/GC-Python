73475319c2e04bc47b5a2ba0349c7d77585bace9
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2023-11-21 12:09:22 +0100

    PFC sans condition

--- PFS.py
+++ PFS.py
@@ -1,40 +1,39 @@
 import random
-values: dict = {"pierre":"ciseaux","feuille":"pierre","ciseaux":"feuille"}
+#[pierre[pierre,feuille,ciseaux],feuille[pierre,feuille,ciseaux],ciseaux[pierre,feuille,ciseaux]]
+values: list = [["égalité","vous avez perdu, dommage !","vous avez gagné !"],
+                ["vous avez gagné !","égalité","vous avez perdu, dommage !"],
+                ["vous avez perdu, dommage !","vous avez gagné !","égalité"]]
+valToInt: dict = {"pierre": 0, "feuille": 1, "ciseaux": 2}
 pc: list = ["pierre","feuille","ciseaux"]
 replay: bool = True
 
 def checkAns(player_input):
-    poss: list = ["pierre","Pierre","feuille","Feuille","ciseaux","Ciseaux"]
+    poss: list = ["pierre","feuille","ciseaux"]
     while True:
         for i in poss:
             if player_input==i:
                 return player_input
         player_input = input("pierre, feuille ou ciseaux ?")
 
-def gameInstance(options):
+def gameInstance(options,listVal):
     
     botChoice = random.choice(options)
-    pChoice: str = input("pierre, feuille ou ciseaux ?")
+    plChoice: str = input("pierre, feuille ou ciseaux ?")
     
-    pChoice = checkAns(pChoice)
+    plChoice = checkAns(plChoice)
     
-    return botChoice,pChoice
+    intBotChoice = listVal[botChoice]
+    plChoice = listVal[plChoice]
+    return botChoice,plChoice,intBotChoice
 
-def play(val,pcChoice,player):
-
-    for i,j in val.items():
-        if player==i and pcChoice==j:
-            print("L'ordinateur a choisi :",pcChoice,"vous avez gagné !")
-        elif player==j and pcChoice==i:
-            print("L'ordinateur a choisi :",pcChoice,"vous avez perdu, dommage !")   
-             
-    if player == pcChoice :
-         print("L'ordinateur a choisi :",pcChoice,"égalité")
+def play(val,pcChoice,player,intPc):
+    
+    print("L'ordinateur a choisi :",pcChoice,val[player][intPc])
 
 while replay : 
     
-    bot,p=gameInstance(pc)
+    bot,p,intBot=gameInstance(pc,valToInt)
     
-    play(values,bot,p)
+    play(values,bot,p,intBot)
          
     replay = input("Voulez-vous rejouer ? y/n ") == "y"
