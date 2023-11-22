22e74665263d595ffdb01306ce37ce6249e1456c
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2023-11-22 16:06:59 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,22 +1,17 @@
-WIN_COMBI = [((0,0),(1,0),(2,0)), ((0,1),(1,1),(2,1)), ((0,2),(1,2),(2,2))]
-gridPlay = [ [" ", " ", " "], [" ", " ", " "], [" ", " ", " "] ]
+WIDTH = 3
+HEIGHT = 3
 
-def updateGrid(grid):
-     return f'[{grid[0][0]}] | [{grid[0][1]}] | [{grid[0][2]}] \n---------------- \n[{grid[1][0]}] | [{grid[1][1]}] | [{grid[1][2]}] \n--------------- \n[{grid[2][0]}] | [{grid[2][1]}] | [{grid[2][2]}]'
+def generateGrid():
+    return [[" "]*WIDTH]*HEIGHT
+    
+gridPlay = generateGrid()
 
-def checkCombinaison(symbol, grid):
-    for c in WIN_COMBI:
-        currentWin = True
-        for pos in c:
-            if grid[pos[0]][pos[1]] != symbol and currentWin == True:
-                currentWin = False
-        if currentWin:
-            return True
-    return False
-
-print(updateGrid(gridPlay))
-while True:
-    position = input("Mettez la position sour la forme 'xy' : ")
-    gridPlay[int(position[0]) - 1][int(position[1]) - 1] = "x"
-    print(updateGrid(gridPlay))
-    print(checkCombinaison("x", gridPlay))
+def printGrid(grid):
+    string = ""
+    for line in grid:
+        for case in line:
+            string += "[" + case + "]"
+        string += "\n"
+    print(string)
+        
+printGrid(gridPlay)
