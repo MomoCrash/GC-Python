d338d94d2e75dc58bcee34f138113ed1d3014697
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2023-11-22 16:07:08 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -4,19 +4,23 @@ gridPlay = [ [" ", " ", " "], [" ", " ", " "], [" ", " ", " "] ]
 def updateGrid(grid):
      return f'[{grid[0][0]}] | [{grid[0][1]}] | [{grid[0][2]}] \n---------------- \n[{grid[1][0]}] | [{grid[1][1]}] | [{grid[1][2]}] \n--------------- \n[{grid[2][0]}] | [{grid[2][1]}] | [{grid[2][2]}]'
 
-def checkCombinaison(symbol, grid):
-    for c in WIN_COMBI:
-        currentWin = True
-        for pos in c:
-            if grid[pos[0]][pos[1]] != symbol and currentWin == True:
-                currentWin = False
-        if currentWin:
-            return True
-    return False
+def checkWin(grid):
+    for i in range(len(grid)-1):
+        for j in range(len(grid[i])-1):
+            if grid[i][j] == grid[i][j+1] and grid[i][j] == grid[i][j-1] and grid[i][j]!=" ":
+                return True,grid[i][j]
+            elif grid[i][j] == grid[i+1][j] and grid[i][j] == grid[i-1][j] and grid[i][j]!=" ":
+                return True,grid[i][j]
+            elif grid[i][j] == grid[i+1][j+1] and grid[i][j] and grid[i-1][j-1]!=" ":
+                return True,grid[i][j]
+            else:
+                return False,""
 
 print(updateGrid(gridPlay))
 while True:
     position = input("Mettez la position sour la forme 'xy' : ")
     gridPlay[int(position[0]) - 1][int(position[1]) - 1] = "x"
     print(updateGrid(gridPlay))
-    print(checkCombinaison("x", gridPlay))
+    isWon, winner=checkWin(gridPlay)
+    if isWon:
+        print(winner,"a gagné")
