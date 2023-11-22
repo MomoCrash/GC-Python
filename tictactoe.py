fcbde5078053d518bd8b949add90d40f867609bf
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2023-11-22 15:21:49 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,40 +1,22 @@
-import random
-values: dict = {"pierre":"ciseaux","feuille":"pierre","ciseaux":"feuille"}
-pc: list = ["pierre","feuille","ciseaux"]
-replay: bool = True
+WIN_COMBI = [((0,0),(1,0),(2,0)), ((0,1),(1,1),(2,1)), ((0,2),(1,2),(2,2))]
+gridPlay = [ [" ", " ", " "], [" ", " ", " "], [" ", " ", " "] ]
 
-def checkAns(player_input):
-    poss: list = ["pierre","Pierre","feuille","Feuille","ciseaux","Ciseaux"]
-    while True:
-        for i in poss:
-            if player_input==i:
-                return player_input
-        player_input = input("pierre, feuille ou ciseaux ?")
+def updateGrid(grid):
+     return f'[{grid[0][0]}] | [{grid[0][1]}] | [{grid[0][2]}] \n---------------- \n[{grid[1][0]}] | [{grid[1][1]}] | [{grid[1][2]}] \n--------------- \n[{grid[2][0]}] | [{grid[2][1]}] | [{grid[2][2]}]'
 
-def gameInstance(options):
-    
-    botChoice = random.choice(options)
-    pChoice: str = input("pierre, feuille ou ciseaux ?")
-    
-    pChoice = checkAns(pChoice)
-    
-    return botChoice,pChoice
+def checkCombinaison(symbol, grid):
+    for c in WIN_COMBI:
+        currentWin = True
+        for pos in c:
+            if grid[pos[0]][pos[1]] != symbol and currentWin == True:
+                currentWin = False
+        if currentWin:
+            return True
+    return False
 
-def play(val,pcChoice,player):
-
-    for i,j in val.items():
-        if player==i and pcChoice==j:
-            print("L'ordinateur a choisi :",pcChoice,"vous avez gagné !")
-        elif player==j and pcChoice==i:
-            print("L'ordinateur a choisi :",pcChoice,"vous avez perdu, dommage !")   
-             
-    if player == pcChoice :
-         print("L'ordinateur a choisi :",pcChoice,"égalité")
-
-while replay : 
-    
-    bot,p=gameInstance(pc)
-    
-    play(values,bot,p)
-         
-    replay = input("Voulez-vous rejouer ? y/n ") == "y"
+print(updateGrid(gridPlay))
+while True:
+    position = input("Mettez la position sour la forme 'xy' : ")
+    gridPlay[int(position[0]) - 1][int(position[1]) - 1] = "x"
+    print(updateGrid(gridPlay))
+    print(checkCombinaison("x", gridPlay))
