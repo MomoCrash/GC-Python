39c2d44d541b24129819b9368ec1b1a130ffce76
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2023-11-22 15:20:42 +0100

    avancée

--- Wordle.py
+++ Wordle.py
@@ -1,23 +1,36 @@
+import random
+
 def checkSize(ask,alphabet)->str:
+    var:str=""
     while len(var) !=5:
         var= input(ask)
     while True :
-        for j in var:
-            if j in alphabet:
+        if all (j in alphabet for j in var):
                 return var.upper()
-        print("Utilisez seulement des lettres de l'alphabet français")
+        print("Utilisez seulement des lettres de l'alphabet")
         var= input(ask)
 
-def checkLetter(guessLetter,tryLetter,guessNum,tryNum):
+def checkLetter(guessLetter,tryLetter,guessNum,tryNum)->str:
     if guessLetter == tryLetter and guessNum == tryNum:
-        
+        return '\033[0;32m'
+    elif guessLetter==tryLetter and guessNum != tryNum:
+        return "\033[38;2;255;165;0m"
+    else :
+        return "\033[1;38m"
     
     
 a: list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 word: str = "PATES"
-playerWord: str = checkSize("Écrivez un mot de 5 lettres",a)
 
+
+playerWord: str = checkSize("Écrivez un mot de 5 lettres : ",a)
+checkList:list = []
 
 for i in range (len(word)):
     for j in range (len(playerWord)):
-        checkLetter(word[i],word[j],i,j)
+        checkList.append(checkLetter(word[i],word[j],i,j))
+
+print(checkList)
+for i in range(len(playerWord)):
+    print(checkList[i],playerWord[i],'\x1b[0m')
+    
---
+++ test.py
@@ -0,0 +1,3 @@
+print('\033[0;32m' + 'Success!' + '\x1b[0m')#vert
+print("\033[38;2;255;165;0m"+'aaa'+'\x1b[0m')#orange
+print("\033[1;38m"+'aaa'+'\x1b[0m')#blanc
