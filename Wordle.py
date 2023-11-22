b6677e594146b40c416092184177407e3edf7752
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2023-11-22 14:18:54 +0100

    début

--- Wordle.py
+++ Wordle.py
@@ -1,10 +1,23 @@
-def verif_taille(demande):
+def checkSize(ask,alphabet)->str:
     while len(var) !=5:
-        var= input(demande)
+        var= input(ask)
+    while True :
+        for j in var:
+            if j in alphabet:
+                return var.upper()
+        print("Utilisez seulement des lettres de l'alphabet français")
+        var= input(ask)
+
+def checkLetter(guessLetter,tryLetter,guessNum,tryNum):
+    if guessLetter == tryLetter and guessNum == tryNum:
+        
+    
+    
+a: list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+word: str = "PATES"
+playerWord: str = checkSize("Écrivez un mot de 5 lettres",a)
 
 
-mot = "pates"
-motJoueur = verif_taille("Écrivez un mot de 5 lettres")
-
-
-for i in mot 
+for i in range (len(word)):
+    for j in range (len(playerWord)):
+        checkLetter(word[i],word[j],i,j)
